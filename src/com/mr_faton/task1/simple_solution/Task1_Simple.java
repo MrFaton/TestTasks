518ae304252436b117a7291e3810b6e2db5406e8
Mr_Faton <firefly90@inbox.ru>
2015-06-15 13:08:58 +0300

    update from home

--- src/com/mr_faton/task1/simple_solution/Task1_Simple.java
+++ src/com/mr_faton/task1/simple_solution/Task1_Simple.java
@@ -6,20 +6,28 @@ import java.util.Scanner;
 
 public class Task1_Simple {
     public static void main(String[] args) {
+        //requirement number of brackets (The N in Your task)
         int openBracketsCount = 0;
+        //collection for keeping brackets expressions
         Collection<String> bracketsExpressions = new ArrayList<>();
+        //tool for input brackets expressions and brackets number
         Scanner scanner = new Scanner(System.in);
 
+        //invite user to input brackets expression. User must to input "stop" for breaking collecting expressions
         System.out.println("Please input brackets expressions (to stop input enter \"stop\"):");
+        //loop until user has entered stop
         while (true) {
             String expression = scanner.next();
             if (expression.equals("stop")) {
                 break;
             }
+            //adding user's expression to collection
             bracketsExpressions.add(expression);
         }
 
+        //invite user to input number of open brackets
         System.out.println("Please input number of open brackets:");
+        //working until user's input number less or equals zero
         while (openBracketsCount <= 0){
             String numOfBracketsStr = scanner.next();
             try {
@@ -28,22 +36,29 @@ public class Task1_Simple {
                     System.out.println("You have entered negative or zero argument, please input another argument:");
                     openBracketsCount = 0;
                 }
+                //exception throw when user input not an integer number
             } catch (NumberFormatException exception) {
                 System.out.println("You have entered bad argument, please try again:");
             }
         }
 
+        //handle every expression
         for (String expression : bracketsExpressions) {
-            System.out.println(eval(openBracketsCount, expression));
+            if (eval(openBracketsCount, expression)) {
+                System.out.println("Satisfying expression: " + expression);
+            }
         }
     }
 
     private static boolean eval(int receivedOpenBrackets, String expression) {
+        //if condition is false then it's means user input not correct expression and answer is false
         if (expression.startsWith("(") && expression.endsWith(")")) {
             int findOpenBrackets = 0;
             int findCloseBrackets = 0;
+            //position of current bracket
             int charPos = 0;
 
+            //count open brackets until user's number of open brackets or until end of expression
             while (findOpenBrackets < receivedOpenBrackets && charPos < expression.length()) {
                 char bracket = expression.charAt(charPos);
                 if (bracket == '(') {
@@ -52,6 +67,7 @@ public class Task1_Simple {
                 charPos++;
             }
 
+            //count close brackets from last open bracket position until num of found open brackets or until end of expr
             while (findCloseBrackets < findOpenBrackets && charPos < expression.length()) {
                 char bracket = expression.charAt(charPos);
                 if (bracket == ')') {
@@ -62,6 +78,7 @@ public class Task1_Simple {
                 charPos++;
             }
 
+            //if num of found open brackets equals num of closed brackets then expr is satisfied
             if (findOpenBrackets == findCloseBrackets) {
                 return true;
             }
